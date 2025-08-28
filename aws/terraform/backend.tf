8c3bab27cf12241b2308a836c7c073fd2d283a34
augustojoselg <augustojoselg@gmail.com>
2025-08-28 18:06:03 -0300

    security: remover backend.tf com dados sensíveis

--- aws/terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-# state.tf
-terraform {
-  backend "s3" {
-    bucket  = "terraform-state-augustojoselg"
-    key     = "aws/terraform.tfstate"
-    region  = "us-east-2"
-    encrypt = true
-  }
-}
